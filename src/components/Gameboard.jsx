3e332be1097742f11e0ccfa1173211e2f89ba91c
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-04-29 09:43:24 +0200

    Implement clicked state for cards and score tracking

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 export default function Gameboard(props) {
+  const [clickedCards, setClickedCards] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  const handleClick = (name) => {
+    if (clickedCards.includes(name)) {
+      if (currentScore > highScore) {
+        setHighScore(currentScore);
+      }
+      setCurrentScore(0);
+      setClickedCards([]);
+    } else {
+      setCurrentScore(currentScore + 1);
+      setClickedCards([...clickedCards, name]);
+    }
+  };
+
   return (
-    <div className="flex gap-4 flex-wrap w-3/4">
-      {props.cardList.map((prop) => {
-        return (
-          <Card
-            key={prop.name}
-            name={prop.name}
-            description={prop.description}
-          />
-        );
-      })}
-    </div>
+    <>
+      <p>Score: {currentScore}</p>
+      <div className="flex gap-4 flex-wrap w-3/4">
+        {props.cardList.map((prop) => {
+          return (
+            <Card
+              key={prop.name}
+              name={prop.name}
+              description={prop.description}
+              onClick={handleClick}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
