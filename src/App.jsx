0bf3768be549ea82b0a505a1641f385393951225
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-04-29 11:40:28 +0200

    Remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Card from "./components/Card";
 import Gameboard from "./components/Gameboard";
 
 function App() {
